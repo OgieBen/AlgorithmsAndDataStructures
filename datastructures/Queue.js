e52f8a36e627ea5666e2ad4a54f80bb4323183bf
ogie Ben <bensoft2k5@gmail.com>
2018-12-18 18:27:41 +0100

    folder restructure

--- datastructures/Queue.js
+++ datastructures/Queue.js
@@ -28,3 +28,25 @@ let Queue = {
          
     },
 }
+
+
+// quick test
+let q = Queue;
+
+q.add(9)
+q.add(4);
+q.add(6);
+q.add(5);
+q.add(7);
+q.remove();
+q.remove();
+q.remove();
+let next = q.front;
+do{
+    console.log(next.val)
+    // if(next.prev){
+    //     let prev = next.prev.val;
+    //     console.log('prev => ' + prev)
+    // }
+    next = next.next;
+}while(next);
