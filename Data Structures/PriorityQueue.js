080548ea6d94cd54bb228863d97f93f67d8deed7
ogie Ben <bensoft2k5@gmail.com>
2018-12-18 18:14:20 +0100

    added remove method

--- Data Structures/PriorityQueue.js
+++ Data Structures/PriorityQueue.js
@@ -4,17 +4,23 @@ let Queue = {
     QueueItem: function (){
         let next = undefined;
         let val = undefined;
+        let prev = undefined;
     },  
+    remove: function () {
+        let rearPrev = this.rear.prev;
+        rearPrev.next = undefined;
+        this.rear = rearPrev;
+    },
     add: function (val) {
          let q = new this.QueueItem();
          q.val = val;
 
-         if (this.front){
-             if(this.rear)
-                this.rear.next = q;
-             this.rear = q;
-         }
-
+        if(this.rear){
+            this.rear.next = q;
+            q.prev = this.rear;
+            this.rear = q;
+        }
+         
          if(!this.front){
             this.front = q;
             this.rear = q;
@@ -30,9 +36,16 @@ q.add(4);
 q.add(6);
 q.add(5);
 q.add(7);
+q.remove();
+q.remove();
+q.remove();
 let next = q.front;
 do{
     console.log(next.val)
+    // if(next.prev){
+    //     let prev = next.prev.val;
+    //     console.log('prev => ' + prev)
+    // }
     next = next.next;
 }while(next);
 
