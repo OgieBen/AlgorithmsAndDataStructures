ba9887a717d146369bfef19da96f027e25843e4c
ogie Ben <bensoft2k5@gmail.com>
2018-12-19 21:43:29 +0100

    added relevant methods for PriorityQueue Object

--- library/datastructures/PriorityQueue.js
+++ library/datastructures/PriorityQueue.js
@@ -1,5 +1,28 @@
 // TODO:: PriorityQueue implementation
 
-let PriorityQueue = {
-    
+const Heap = require('./Heap');
+
+let PriorityQueue ={
+    heapMaximum :function () {
+
+    }, 
+
+    heapMaximumExtract: function() {
+
+    },
+
+    heapIncreaseKey: function() {
+         
+    },
+
+    heapInsertKey: function() {
+
+    }
 }
+
+let heap = Heap;
+// let data = [4,1,3,2,16,9,10,14,8,7];
+let data = [23, 17, 14, 6, 13, 10, 1, 5, 7, 12];
+// heap.buildMaxHeap(data);
+let sorted = heap.sort(data);
+console.log(sorted.values);
